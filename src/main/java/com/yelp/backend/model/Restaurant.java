1d46c23dcaa7c53253cccec83448719d777f787c
Shreyas <helloshreyasj@gmail.com>
2026-06-03 16:30:41 +0400

    chore: add project files

---
+++ src/main/java/com/yelp/backend/model/Restaurant.java
@@ -0,0 +1,28 @@
+package com.yelp.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="restaurants")
+@Data
+public class Restaurant {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String name;
+	
+	@Column(nullable = false)
+	private String location;
+	
+	@Column(name = "price_range", nullable = false, length = 5)
+	private int priceRange;
+}
